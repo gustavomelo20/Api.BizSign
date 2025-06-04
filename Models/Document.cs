fa82ff74832facc9feb512cd61cfffff0b4df9db
gustavomelo20 <gustavo.melo@origoenergia.com.br>
2025-06-04 17:46:49 -0300

    fix: lint

--- Models/Document.cs
+++ Models/Document.cs
@@ -4,15 +4,10 @@ public class Document
 {
     public Guid Id { get; set; }
     public string? OwnerID { get; set; }
-    
     public string Title { get; set; }
-    
     public string Description { get; set; }
-    
     public string Status { get; set; } = string.Empty;
-    
-    public  List<Signatory> Signatories { get; set; }
-    public string File { get; set; } = string.Empty; 
-    
+    public List<Signatory> Signatories { get; set; }
+    public string File { get; set; } = string.Empty;
     public int Progress { get; set; }
 }
--- Models/Signatory.cs
+++ Models/Signatory.cs
@@ -3,14 +3,9 @@
 public class Signatory
 {
     public Guid Id { get; set; }
-    
     public string Name { get; set; } = string.Empty;
-    
-    public string Email { get; set; } = string.Empty; 
-    
-    public string Phone { get; set; } = string.Empty; 
-    
-    public string Role { get; set; } = string.Empty; 
-    
-    public string Company { get; set; } = string.Empty; 
+    public string Email { get; set; } = string.Empty;
+    public string Phone { get; set; } = string.Empty;
+    public string Role { get; set; } = string.Empty;
+    public string Company { get; set; } = string.Empty;
 }
